6106ed63c11c03c322c8f06e9a170dcb7f653b84
YeasirArafatAyan <yeasirarafat.ayan@mnsu.edu>
2025-09-29 16:30:09 -0500

    First Push
    First Push

---
+++ Assets/startforce.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartForce : MonoBehaviour
+{
+    public Vector3 startForce;
+
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(startForce, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+
+    }
+}
